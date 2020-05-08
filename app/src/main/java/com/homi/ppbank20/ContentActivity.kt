fbfe07569ee8cdd86025bf1f4e1a4ac733895ada
wells <jhang0912407249@gmail.com>
2020-05-08 12:09:56 +0800

    bottomnavmenu finish

--- app/src/main/java/com/homi/ppbank20/ContentActivity.kt
+++ app/src/main/java/com/homi/ppbank20/ContentActivity.kt
@@ -2,11 +2,41 @@ package com.homi.ppbank20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Switch
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.activity_content.*
+import kotlinx.android.synthetic.main.fragment_account.*
 
 class ContentActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_content)
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val navController=findNavController(R.id.navfragment)
+        bottomNavigationView.setOnNavigationItemSelectedListener { item: MenuItem ->
+            when (item.itemId) {
+                R.id.menu_account -> {
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.menu_apply -> {
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.menu_task -> {
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.menu_more -> {
+                    return@setOnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 }
